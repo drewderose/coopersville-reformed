af7562c3d26fbd6020ef7fcbf8162628e636354c
drewderose <159578967+drewderose@users.noreply.github.com>
2024-04-18 17:26:41 -0400

    Update commitmentcard.js

--- commitmentcard.js
+++ commitmentcard.js
@@ -1,33 +1,35 @@
 const myInterval = setInterval(adderFunc, 100);
 
 function adderFunc() {
-  var annual = Number(document.getElementsByName("5urH45uOzZQ5sZfaRC08")[0].value);
-  var quarter = Number(document.getElementsByName("gFpDPwgZbbLH54HoaR4H")[0].value);
-  var month = Number(document.getElementsByName("J6EzsPVs3AVlpxTnhkgC")[0].value);
-  var week = Number(document.getElementsByName("Ls19OFmpZFUA32xGoHac")[0].value);
-  var random = Number(document.getElementsByName("ORqwJ5WQRQsRshrAJvkX")[0].value);
-  var initial = Number(document.getElementsByName("gkRhOZYwm2ZPRYyPmFdq")[0].value);
-  var total = (annual*3) + (quarter*12) + (month*36) + (week*156) + random + initial;
-  let USDollar = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  });
-  document.getElementById("total-contribution").innerText = USDollar.format(total);
-  document.getElementsByName("UyOfXvinODfpfmr42qTP")[0].value = total;
-  document.getElementsByName("UyOfXvinODfpfmr42qTP")[0].dispatchEvent(new Event("input", {
-    bubbles: true
-  }));
+  var x = document.getElementsByName("5urH45uOzZQ5sZfaRC08")[0];
+  if (x === undefined || x === null ) {} else {
+    var annual = Number(document.getElementsByName("5urH45uOzZQ5sZfaRC08")[0].value);
+    var quarter = Number(document.getElementsByName("gFpDPwgZbbLH54HoaR4H")[0].value);
+    var month = Number(document.getElementsByName("J6EzsPVs3AVlpxTnhkgC")[0].value);
+    var week = Number(document.getElementsByName("Ls19OFmpZFUA32xGoHac")[0].value);
+    var random = Number(document.getElementsByName("ORqwJ5WQRQsRshrAJvkX")[0].value);
+    var initial = Number(document.getElementsByName("gkRhOZYwm2ZPRYyPmFdq")[0].value);
+    var total = (annual*3) + (quarter*12) + (month*36) + (week*156) + random + initial;
+    let USDollar = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+    document.getElementById("total-contribution").innerText = USDollar.format(total);
+    document.getElementsByName("UyOfXvinODfpfmr42qTP")[0].value = total;
+    document.getElementsByName("UyOfXvinODfpfmr42qTP")[0].dispatchEvent(new Event("input", {
+      bubbles: true
+    }));
+    document.querySelector("input[value='Annually']").addEventListener('click', clearFunc);
+    document.querySelector("input[value='Quarterly']").addEventListener('click', clearFunc);
+    document.querySelector("input[value='Monthly']").addEventListener('click', clearFunc);
+    document.querySelector("input[value='Weekly']").addEventListener('click', clearFunc);
+    document.querySelector("input[value='Non-traditional (RMD, Stocks, Bonds, assets, etc.)']").addEventListener('click', clearFunc);
+  }
 }
-
-document.querySelector("input[value='Annually']").addEventListener('click', clearFunc);
-document.querySelector("input[value='Quarterly']").addEventListener('click', clearFunc);
-document.querySelector("input[value='Monthly']").addEventListener('click', clearFunc);
-document.querySelector("input[value='Weekly']").addEventListener('click', clearFunc);
-document.querySelector("input[value='Non-traditional (RMD, Stocks, Bonds, assets, etc.)']").addEventListener('click', clearFunc);
 
 function clearFunc() {
   var zero = Number(0);
-  document.getElementsByName("5urH45uOzZQ5sZfaRC08")[0].value = zero;
+  document.getElementsByName("5urH45uOzZQ5sZfaRC08")[0].value = 200;
   document.getElementsByName("gFpDPwgZbbLH54HoaR4H")[0].value = zero;
   document.getElementsByName("J6EzsPVs3AVlpxTnhkgC")[0].value = zero;
   document.getElementsByName("Ls19OFmpZFUA32xGoHac")[0].value = zero;
